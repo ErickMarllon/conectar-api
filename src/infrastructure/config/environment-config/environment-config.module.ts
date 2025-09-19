a99c8d523a6546ef908718f3383d50d175776ef1
Erick Carvalho <erick.carvalho@teamsoft.com.br>
2025-09-19 15:45:22 -0300

    chore: update project dependencies and configs

--- src/infrastructure/config/environment-config/environment-config.module.ts
+++ src/infrastructure/config/environment-config/environment-config.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 import appConfig from './app.config';
 import awsConfig from './aws.config';
 import databaseConfig from './database.config';
@@ -7,6 +9,10 @@ import googleConfig from './google.config';
 import jwtConfig from './jwt.config';
 import metaConfig from './meta.config';
 import webConfig from './web.config';
+
+if (fs.existsSync('/etc/secrets/.env')) {
+  dotenv.config({ path: '/etc/secrets/.env' });
+}
 
 const envFile = process.env.NODE_ENV
   ? `env/.env.${process.env.NODE_ENV}`
--- src/infrastructure/database/pgsql/data-source.ts
+++ src/infrastructure/database/pgsql/data-source.ts
@@ -1,14 +1,10 @@
 import * as migrations from '@/infrastructure/database/migrations';
 import * as entities from '@/infrastructure/database/pgsql/entities';
-import * as dotenv from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederFactoryItem, SeederOptions } from 'typeorm-extension';
 
 import * as seedsModules from './seeds';
 import * as factoriesModules from './seeds/factories';
-
-const NODE_ENV = process.env.NODE_ENV;
-dotenv.config({ path: `./env/.env.${NODE_ENV}` });
 
 const seeds = Object.values(seedsModules);
 const factories: SeederFactoryItem[] = Object.values(
