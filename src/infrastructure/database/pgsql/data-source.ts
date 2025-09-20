64335a1e93b0a16b2daf720f53585504b8b89fda
Erick Carvalho <erick.carvalho@teamsoft.com.br>
2025-09-19 22:46:32 -0300

    chore: update project dependencies and configs

--- src/infrastructure/database/pgsql/data-source.ts
+++ src/infrastructure/database/pgsql/data-source.ts
@@ -13,12 +13,12 @@ const factories: SeederFactoryItem[] = Object.values(
 
 const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT ?? '5432'),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
   url: process.env.DATABASE_URL,
+  // host: process.env.DATABASE_HOST,
+  // port: parseInt(process.env.DATABASE_PORT ?? '5432'),
+  // username: process.env.DATABASE_USER,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE_NAME,
   synchronize: false,
   logging: false,
   schema: 'public',
